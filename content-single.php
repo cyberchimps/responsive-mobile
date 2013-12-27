4c87fe73a5ebcf8b659785b9cf541286ade9097a
Ulrich Pogson <grapplerulrich@gmail.com>
2013-12-27 18:53:03 +0100

    Commit _responsive

---
+++ content-single.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @package Responsive
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+
+		<div class="entry-meta">
+			<?php responsive_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'responsive' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-meta">
+		<?php
+			/* translators: used between list items, there is a space after the comma */
+			$category_list = get_the_category_list( __( ', ', 'responsive' ) );
+
+			/* translators: used between list items, there is a space after the comma */
+			$tag_list = get_the_tag_list( '', __( ', ', 'responsive' ) );
+
+			if ( ! responsive_categorized_blog() ) {
+				// This blog only has 1 category so we just need to worry about tags in the meta text
+				if ( '' != $tag_list ) {
+					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'responsive' );
+				} else {
+					$meta_text = __( 'Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'responsive' );
+				}
+
+			} else {
+				// But this blog has loads of categories so we should probably display them here
+				if ( '' != $tag_list ) {
+					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'responsive' );
+				} else {
+					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'responsive' );
+				}
+
+			} // end check for categories on this blog
+
+			printf(
+				$meta_text,
+				$category_list,
+				$tag_list,
+				get_permalink()
+			);
+		?>
+
+		<?php edit_post_link( __( 'Edit', 'responsive' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-meta -->
+</article><!-- #post-## -->
