de37a5b031edfcf4bc4564cdbde048f3e9592ee7
Ulrich Pogson <grapplerulrich@gmail.com>
2014-01-23 23:18:59 +0100

    Inlcude page and function doc standard

---
+++ core/standard.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Full Content Template
+ *
+ * Template Name:  Full Width Page (no sidebar)
+ *
+ * @package        Responsive
+ * @license        license.txt
+ * @copyright      2014 CyberChimps
+ * @since          1.0.0
+ */
+
+/*
+ * Add notification to Reading Settings page to notify if Custom Front Page is enabled.
+ *
+ * @since    1.9.4.0
+ */
