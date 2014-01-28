6f60fa4f876ec6351f3a6d21d130dc72c04d55ea
Hugo Saner <hugo@sanerdesign.com>
2014-01-28 16:56:57 +0000

    Starting re-coding the css and structure

--- header.php
+++ header.php
@@ -7,16 +7,16 @@
  * @package Responsive
  */
 ?>
-<!DOCTYPE html>
-<!--[if !IE]>
-<html class="no-js non-ie" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 7 ]>
-<html class="no-js ie7" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 8 ]>
-<html class="no-js ie8" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 9 ]>
-<html class="no-js ie9" <?php language_attributes(); ?>> <![endif]-->
-<!--[if gt IE 9]><!-->
+	<!DOCTYPE html>
+	<!--[if !IE]>
+	<html class="no-js non-ie" <?php language_attributes(); ?>> <![endif]-->
+	<!--[if IE 7 ]>
+	<html class="no-js ie7" <?php language_attributes(); ?>> <![endif]-->
+	<!--[if IE 8 ]>
+	<html class="no-js ie8" <?php language_attributes(); ?>> <![endif]-->
+	<!--[if IE 9 ]>
+	<html class="no-js ie9" <?php language_attributes(); ?>> <![endif]-->
+	<!--[if gt IE 9]><!-->
 <html <?php language_attributes(); ?>>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
@@ -33,35 +33,41 @@
 <body <?php body_class(); ?>>
 <?php responsive_container(); // before container hook ?>
 <div id="container" class="hfeed site">
-	<?php do_action( 'before' ); ?>
-	<?php responsive_header(); // before header hook ?>
-	<header id="header" class="site-header" role="banner">
-		<?php responsive_header_top(); // before header content hook ?>
-		<?php if( has_nav_menu( 'top-menu', 'responsive' ) ) {
-			wp_nav_menu(
-				array(
-					'container'      => '',
-					'fallback_cb'    => false,
-					'menu_class'     => 'top-menu',
-					'theme_location' => 'top-menu'
-				)
-			);
-		} ?>
-		<?php responsive_in_header(); // header hook ?>
-		<div id="logo" class="site-branding">
-			<?php if ( get_header_image() ) : ?>
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-					<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
-				</a>
-			<?php endif; // End header image check. ?>
+<?php do_action( 'before' ); ?>
+	<nav id="top-menu" class="container-full-width">
+		<div class="container">
+			<?php responsive_header_top(); // before header content hook ?>
+			<?php if ( has_nav_menu( 'top-menu', 'responsive' ) ) {
+				wp_nav_menu(
+					array(
+						'container'      => '',
+						'fallback_cb'    => false,
+						'menu_class'     => 'top-menu',
+						'theme_location' => 'top-menu',
+						'depth'          => 1
+					)
+				);
+			} ?>
 		</div>
-		<div id="site-header-text" class="site-branding">
-			<?php if ( display_header_text() ) : ?>
-				<h1 class="site-name"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			<?php endif; // End header text check. ?>
+	</nav>
+<?php responsive_header(); // before header hook ?>
+	<header id="header" class="container-full-width site-header" role="banner">
+		<div class="container">
+			<?php responsive_in_header(); // header hook ?>
+			<div id="logo" class="site-branding">
+				<?php if ( get_header_image() ) : ?>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
+					</a>
+				<?php endif; // End header image check. ?>
+			</div>
+			<div id="site-header-text" class="site-branding">
+				<?php if ( display_header_text() ) : ?>
+					<h2 class="site-name"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h2>
+					<h3 class="site-description"><?php bloginfo( 'description' ); ?></h3>
+				<?php endif; // End header text check. ?>
+			</div>
 		</div>
-
 
 		<?php get_sidebar( 'top' ); ?>
 
@@ -77,12 +83,13 @@
 					'theme_location'  => 'header-menu'
 				)
 			); ?>
-		</nav><!-- #site-navigation -->
+		</nav>
+		<!-- #site-navigation -->
 		<?php responsive_header_bottom(); // after header content hook ?>
 	</header><!-- #header -->
-	<?php responsive_header_end(); // after header container hook ?>
+<?php responsive_header_end(); // after header container hook ?>
 
-	<?php responsive_wrapper(); // before wrapper container hook ?>
-	<div id="wrapper" class="row site-content clearfix">
-	<?php responsive_wrapper_top(); // before wrapper content hook ?>
-	<?php responsive_in_wrapper(); // wrapper hook ?>
+<?php responsive_wrapper(); // before wrapper container hook ?>
+	<div id="wrapper" class="row site-content">
+<?php responsive_wrapper_top(); // before wrapper content hook ?>
+<?php responsive_in_wrapper(); // wrapper hook ?>
