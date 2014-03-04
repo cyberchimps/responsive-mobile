e051245926e7064d6bdd33ff56a06dbdd5c8b885
Ulrich Pogson <grapplerulrich@gmail.com>
2014-03-04 23:20:34 +0100

    Update tha-theme-hooks

--- core/includes/tha-theme-hooks.php
+++ core/includes/tha-theme-hooks.php
@@ -1,22 +1,22 @@
 <?php
 /**
- * Theme Hook Alliance hook stub list.
- *
- * @package        themehookalliance
- * @version        1.0-draft
- * @since        1.0-draft
- * @license        http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, v2 (or newer)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+* Theme Hook Alliance hook stub list.
+*
+* @package 		themehookalliance
+* @version		1.0-draft
+* @since		1.0-draft
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, v2 (or newer)
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
 
 /**
  * Define the version of THA support, in case that becomes useful down the road.
@@ -29,11 +29,11 @@ define( 'THA_HOOKS_VERSION', '1.0-draft' );
  *
  * Example:
  * <code>
- *        // Declare support for all hook types
- *        add_theme_support( 'tha_hooks', array( 'all' ) );
+ * 		// Declare support for all hook types
+ * 		add_theme_support( 'tha_hooks', array( 'all' ) );
  *
- *        // Declare support for certain hook types only
- *        add_theme_support( 'tha_hooks', array( 'header', 'content', 'footer' ) );
+ * 		// Declare support for certain hook types only
+ * 		add_theme_support( 'tha_hooks', array( 'header', 'content', 'footer' ) );
  * </code>
  */
 add_theme_support( 'tha_hooks', array(
@@ -54,6 +54,8 @@ add_theme_support( 'tha_hooks', array(
 	 * When the 'all' parameter was set, specific hook types do not need to be
 	 * added explicitly.
 	 */
+	'html',
+	'body',
 	'head',
 	'header',
 	'content',
@@ -79,8 +81,8 @@ add_theme_support( 'tha_hooks', array(
  *
  * Example:
  * <code>
- *        if ( current_theme_supports( 'tha_hooks', 'header' ) )
- *            add_action( 'tha_head_top', 'prefix_header_top' );
+ * 		if ( current_theme_supports( 'tha_hooks', 'header' ) )
+ * 	  		add_action( 'tha_head_top', 'prefix_header_top' );
  * </code>
  *
  * @param bool $bool true
@@ -92,14 +94,34 @@ add_theme_support( 'tha_hooks', array(
 function tha_current_theme_supports( $bool, $args, $registered ) {
 	return in_array( $args[0], $registered[0] ) || in_array( 'all', $registered[0] );
 }
-
 add_filter( 'current_theme_supports-tha_hooks', 'tha_current_theme_supports', 10, 3 );
 
 /**
- * HTML <head> hooks
- *
- * $tha_supports[] = 'head';
+ * HTML <html> hook
+ * Special case, useful for <DOCTYPE>, etc.
+ * $tha_supports[] = 'html;
  */
+ function tha_html_before() {
+	 do_action( 'tha_html_before' );
+ }
+
+/**
+ * HTML <body> hooks
+ * $tha_supports[] = 'body';
+ */
+ function tha_body_top() {
+	 do_action( 'tha_body_top' );
+ }
+
+ function tha_body_bottom() {
+	 do_action( 'tha_body_bottom' );
+ }
+
+/**
+* HTML <head> hooks
+*
+* $tha_supports[] = 'head';
+*/
 function tha_head_top() {
 	do_action( 'tha_head_top' );
 }
@@ -109,10 +131,10 @@ function tha_head_bottom() {
 }
 
 /**
- * Semantic <header> hooks
- *
- * $tha_supports[] = 'header';
- */
+* Semantic <header> hooks
+*
+* $tha_supports[] = 'header';
+*/
 function tha_header_before() {
 	do_action( 'tha_header_before' );
 }
@@ -130,10 +152,10 @@ function tha_header_bottom() {
 }
 
 /**
- * Semantic <content> hooks
- *
- * $tha_supports[] = 'content';
- */
+* Semantic <content> hooks
+*
+* $tha_supports[] = 'content';
+*/
 function tha_content_before() {
 	do_action( 'tha_content_before' );
 }
@@ -151,10 +173,10 @@ function tha_content_bottom() {
 }
 
 /**
- * Semantic <entry> hooks
- *
- * $tha_supports[] = 'entry';
- */
+* Semantic <entry> hooks
+*
+* $tha_supports[] = 'entry';
+*/
 function tha_entry_before() {
 	do_action( 'tha_entry_before' );
 }
@@ -172,10 +194,10 @@ function tha_entry_bottom() {
 }
 
 /**
- * Comments block hooks
- *
- * $tha_supports[] = 'comments';
- */
+* Comments block hooks
+*
+* $tha_supports[] = 'comments';
+*/
 function tha_comments_before() {
 	do_action( 'tha_comments_before' );
 }
@@ -185,10 +207,10 @@ function tha_comments_after() {
 }
 
 /**
- * Semantic <sidebar> hooks
- *
- * $tha_supports[] = 'sidebar';
- */
+* Semantic <sidebar> hooks
+*
+* $tha_supports[] = 'sidebar';
+*/
 function tha_sidebars_before() {
 	do_action( 'tha_sidebars_before' );
 }
@@ -206,10 +228,10 @@ function tha_sidebar_bottom() {
 }
 
 /**
- * Semantic <footer> hooks
- *
- * $tha_supports[] = 'footer';
- */
+* Semantic <footer> hooks
+*
+* $tha_supports[] = 'footer';
+*/
 function tha_footer_before() {
 	do_action( 'tha_footer_before' );
 }
