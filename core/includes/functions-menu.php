b7952f9886bb3ffbd0475db83b1300a4d5681ec9
Ulrich Pogson <grapplerulrich@gmail.com>
2014-03-04 11:58:28 +0100

    Remove responsive_widget_menu_class

--- core/includes/functions-menu.php
+++ core/includes/functions-menu.php
@@ -13,39 +13,6 @@ function responsive_page_menu_args( $args ) {
 add_filter( 'wp_page_menu_args', 'responsive_page_menu_args' );
 
 /**
- * This function removes .menu class from custom menus
- * in widgets only and fallback's on default widget lists
- * and assigns new unique class called .menu-widget
- *
- * Marko Heijnen Contribution
- *
- */
-class responsive_widget_menu_class {
-	public function __construct() {
-		add_action( 'widget_display_callback', array( $this, 'menu_different_class' ), 10, 2 );
-	}
-
-	public function menu_different_class( $settings, $widget ) {
-		if( $widget instanceof WP_Nav_Menu_Widget ) {
-			add_filter( 'wp_nav_menu_args', array( $this, 'wp_nav_menu_args' ) );
-		}
-
-		return $settings;
-	}
-
-	public function wp_nav_menu_args( $args ) {
-		remove_filter( 'wp_nav_menu_args', array( $this, 'wp_nav_menu_args' ) );
-
-		if( 'menu' == $args['menu_class'] ) {
-			$args['menu_class'] = apply_filters( 'responsive_menu_widget_class', 'menu_widget');
-		}
-
-		return $args;
-	}
-}
-$GLOBALS['nav_menu_widget_classname'] = new responsive_widget_menu_class();
-
-/**
  * Set a fallback menu that will show a home link.
  */
 function responsive_fallback_menu() {
