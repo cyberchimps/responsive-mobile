dee2d0dc7d668320a291ba36208f45414457a95f
Simon S <simon@cyberchimps.com>
2014-06-18 11:02:28 +0530

    Added filter to get by_line_comments option working.

--- template-parts/post-meta.php
+++ template-parts/post-meta.php
@@ -28,8 +28,13 @@ if ( ! defined( 'WPINC' ) ) {
 
 	<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="post-meta">
-			<?php responsive_post_meta_data(); ?>
-			<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+			<?php
+			responsive_post_meta_data();
+			
+			// Added filter to get by_line_comments option working.
+			$by_line_comments = apply_filters( 'responsive_by_line_comments', '1' );
+			
+			if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) && $by_line_comments ) : ?>
 				<span class="comments-link">
 					<span class="mdash">&mdash;</span>
 					<?php comments_popup_link( __( 'No Comments &darr;', 'responsive' ), __( '1 Comment &darr;', 'responsive' ), __( '% Comments &darr;', 'responsive' ) ); ?>
