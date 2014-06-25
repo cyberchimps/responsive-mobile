9e5b34968a043178729872e8f852725634031dc9
Ulrich Pogson <grapplerulrich@gmail.com>
2014-06-25 21:45:27 +0200

    Remove extra widget

--- includes/functions-sidebar.php
+++ includes/functions-sidebar.php
@@ -23,15 +23,6 @@ if ( ! defined( 'WPINC' ) ) {
  */
 function responsive_widgets_init() {
 	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'responsive' ),
-		'id'            => 'sidebar-1',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
-
-	register_sidebar( array(
 		'name'          => __( 'Main Sidebar', 'responsive' ),
 		'description'   => __( 'Area 1 - sidebar.php - Displays on Default, Blog, Blog Excerpt page templates', 'responsive' ),
 		'id'            => 'main-sidebar',
