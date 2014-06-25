d6e288763ef8e454ab7baf552311a019d5fdf454
Ulrich Pogson <grapplerulrich@gmail.com>
2014-06-25 07:28:02 +0200

    Improve code layout

--- front-page.php
+++ front-page.php
@@ -17,20 +17,25 @@
 if ( !defined( 'ABSPATH' ) ) {
 	exit;
 }
+
 /**
  * If front page is set to display the
  * blog posts index, include home.php;
  * otherwise, display static front page
  * content
  */
-
 $responsive_options = responsive_get_options();
+
 if ( 'posts' == get_option( 'show_on_front' ) && $responsive_options['front_page'] != 1 ) {
+
 	get_template_part( 'home' );
+
 } elseif ( 'page' == get_option( 'show_on_front' ) && $responsive_options['front_page'] != 1 ) {
+
 	$template = get_post_meta( get_option( 'page_on_front' ), '_wp_page_template', true );
 	$template = ( $template == 'default' ) ? 'index.php' : $template;
 	locate_template( $template, true );
+
 } else {
 
 	get_header();
