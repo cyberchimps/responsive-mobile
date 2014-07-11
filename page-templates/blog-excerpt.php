6357e1b2f8194eafe40db6b9a42fe4a47ff82e4c
Ulrich Pogson <grapplerulrich@gmail.com>
2014-07-11 23:10:19 +0200

    Load page content template-parts for page templates

--- page-templates/blog-excerpt.php
+++ page-templates/blog-excerpt.php
@@ -44,13 +44,7 @@ get_header(); ?>
 				<?php /* Start the Loop */ ?>
 				<?php while ( $blog_query->have_posts() ) : $blog_query->the_post(); ?>
 
-					<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', 'excerpt' );
-					?>
+					<?php get_template_part( 'template-parts/content', 'excerpt' ); ?>
 
 				<?php endwhile; ?>
 
--- page-templates/sitemap.php
+++ page-templates/sitemap.php
@@ -30,13 +30,7 @@ get_header(); ?>
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'template-parts/content', 'sitemap' );
-					?>
+					<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
 				<?php endwhile; ?>
 
