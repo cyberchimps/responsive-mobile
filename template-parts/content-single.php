1119a0d82f394e4f9135b2ae699ee310193ae052
Ulrich Pogson <grapplerulrich@gmail.com>
2014-12-07 22:17:28 +0100

    Add featured image to single post

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -23,6 +23,12 @@ if ( ! defined( 'WPINC' ) ) {
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php responsive_mobile_entry_top(); ?>
 	<?php get_template_part( 'template-parts/post-meta' ); ?>
+	<?php
+	// Added filter to get featured_image option working.
+	$featured_image = apply_filters( 'responsive_mobile_featured_image', '1' );
+	if ( has_post_thumbnail() && $featured_image ) {
+		the_post_thumbnail();
+	} ?>
 
 	<div class="post-entry">
 		<?php the_content(); ?>
