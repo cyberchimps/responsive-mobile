3d9c879116658ee6fd9b72505f6f8d049edf6025
hbwsl Manju <manju.sah@hbwsl.com>
2017-09-18 11:25:25 +0530

    added contact settings

--- includes/functions-theme-options.php
+++ includes/functions-theme-options.php
@@ -483,6 +483,72 @@ function responsive_mobile_theme_options_set()
 					'options'     => Responsive_Options::responsive_mobile_categorylist_validate()
 			)
 			)
+		),
+            'Contact Section'          => array(
+			'title'  => __( 'Contact Section for the homepage', 'responsive-mobile' ),
+			'fields' => array(
+			array(
+					'title'       => __( 'Enable Contact Section', 'responsive-mobile' ),
+					'subtitle'    => '',
+					'heading'     => '',
+					'type'        => 'checkbox',
+					'id'          => 'contact',
+					'default'     => '',
+					'description' => __( 'Check to enable', 'responsive-mobile' ),
+					'placeholder' => ''
+			),
+			array(
+					'title'       => __( 'Contact Section Title', 'responsive-mobile' ),
+					'subtitle'    => '',
+					'heading'     => '',
+					'type'        => 'text',
+					'id'          => 'contact_title',
+					'default'     => '',
+					'description' => __( 'Enter contact section title', 'responsive-mobile' ),
+					'placeholder' => __( 'Get In Touch', 'responsive-mobile' )
+			),
+                        array(
+					'title'       => __( 'Contact Address', 'responsive-mobile' ),
+					'subtitle'    => '',
+					'heading'     => '',
+					'type'        => 'text',
+					'id'          => 'contact_address',
+					'default'     => '',
+					'description' => __( '', 'responsive-mobile' ),
+					'placeholder' => __( 'Sollicitudin Accumsa, hendrerit finibus, Jnibus- 45, Suscipit eleifend. ', 'responsive-mobile' )
+			),
+                            array(
+					'title'       => __( 'Contact Number', 'responsive-mobile' ),
+					'subtitle'    => '',
+					'heading'     => '',
+					'type'        => 'text',
+					'id'          => 'contact_number',
+					'default'     => '',
+					'description' => __( '', 'responsive-mobile' ),
+					'placeholder' => __( '+1 212 555 1234', 'responsive-mobile' )
+			),
+                             array(
+					'title'       => __( 'Contact Email', 'responsive-mobile' ),
+					'subtitle'    => '',
+					'heading'     => '',
+					'type'        => 'text',
+					'id'          => 'contact_email',
+					'default'     => '',
+					'description' => __( '', 'responsive-mobile' ),
+					'placeholder' => __( 'hello@yourdomain', 'responsive-mobile' )
+			),
+                             array(
+					'title'       => __( 'Additional Data', 'responsive-mobile' ),
+					'subtitle'    => '',
+					'heading'     => '',
+					'type'        => 'text',
+					'id'          => 'contact_form',
+					'default'     => '',
+					'description' => __( 'Recommended: Contact Form', 'responsive-mobile' ),
+					'placeholder' => __( '', 'responsive-mobile' )
+			),
+			
+			)
 		)
 
 	);
